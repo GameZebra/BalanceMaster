f76ee97d2a3410d93605f8c1eea8c414a329c8d8
GameZebra <mr.xmitkotox@gmail.com>
2025-02-17 13:13:09 +0200

    reading range

--- initial_tests/GyroRead/Core/Src/main.c
+++ initial_tests/GyroRead/Core/Src/main.c
@@ -47,6 +47,8 @@ UART_HandleTypeDef huart2;
 /* USER CODE BEGIN PV */
 short angle = 0;
 uint16_t adcValue = 0;
+uint16_t adcMin = 3000;
+uint16_t adcMax = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -107,6 +109,10 @@ int main(void)
 	adcValue = HAL_ADC_GetValue(&hadc1);
 	HAL_ADC_Stop(&hadc1);
 
+	if (adcValue < adcMin)
+		adcMin = adcValue;
+	else if (adcValue > adcMax)
+		adcMax = adcValue;
 
 
     /* USER CODE END WHILE */
@@ -148,7 +154,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
